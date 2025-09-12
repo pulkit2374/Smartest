d3230d28f4cced20a9c139bb8812d3a8725282c4
Pulkit Mathur <114744641+pulkit2374@users.noreply.github.com>
2025-09-13 03:28:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
     DB_USERNAME = os.environ.get("MYSQLUSER", "root")          
-    DB_PASSWORD = os.environ.get("MYSQLPASSWORD", "Prime")     
-    DB_HOST = os.environ.get("MYSQLHOST", "localhost")         
-    DB_NAME = os.environ.get("MYSQLDATABASE", "smartest")      
+    DB_PASSWORD = os.environ.get("MYSQLPASSWORD", "DzZFgVkFDzXTzeHwdhavteVdGxjUBEEW")     
+    DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")         
+    DB_NAME = os.environ.get("MYSQLDATABASE", "railway")      
     
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:DzZFgVkFDzXTzeHwdhavteVdGxjUBEEW@mysql.railway.internal/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
