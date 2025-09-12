a1cff07a4a3f367b4578ad94524fc3cce40f2730
Pulkit Mathur <114744641+pulkit2374@users.noreply.github.com>
2025-09-13 03:41:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")         
     DB_NAME = os.environ.get("MYSQLDATABASE", "railway")      
     
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:DzZFgVkFDzXTzeHwdhavteVdGxjUBEEW@mysql.railway.internal/railway"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{root}:{DzZFgVkFDzXTzeHwdhavteVdGxjUBEEW}@{mysql.railway.internal}/{railway}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
